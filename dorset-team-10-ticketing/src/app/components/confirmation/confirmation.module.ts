2816789c47f6fb03315d0bca936ac11a6c600dc2
Chantal Ochiai <chantalochiaionline@gmail.com>
2021-11-23 15:18:31 +0000

    Initial commit.

---
+++ dorset-team-10-ticketing/src/app/components/confirmation/confirmation.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConfirmationRoutingModule } from './confirmation-routing.module';
+import { IonicModule } from '@ionic/angular';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConfirmationRoutingModule,
+    IonicModule,
+    ConfirmationRoutingModule
+  ]
+})
+export class ConfirmationModule { }
